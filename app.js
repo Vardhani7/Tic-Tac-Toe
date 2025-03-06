b61859f3642bc24b94090068548b14883ffdfc9a
Vardhani7 <chsvardhani@gmail.com>
2025-03-06 22:54:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+
+let turnO=true;
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+]
